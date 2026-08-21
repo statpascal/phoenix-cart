6f0068ec18bb78cd9e3c95f0e6167d018c00b47f
statpascal <git@greylags.de>
2026-08-21 13:55:45 +0200

    Blinking cursor

---
+++ resources/convmag.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+# Convert the pattern/color descriptions in Magellan files to 
+# internal data structures linked with the cart
+
+fpc -Mdelphi convmag.pas
+./convmag phoenix-light.mag pattern-light.dat
+./convmag phoenix-dark.mag pattern-dark.dat
